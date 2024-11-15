14aaaacc989df7368ea5e05986b185add8d39306
AsmSafone <77989182+AsmSafone@users.noreply.github.com>
2024-11-15 23:31:41 +0600

    feat: update homepage

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,5 @@
-export interface Env {
-  // Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-  ANALYTICS: KVNamespace;
-  //
-  // Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-  // MY_DURABLE_OBJECT: DurableObjectNamespace;
-  //
-  // Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-  // MY_BUCKET: R2Bucket;
-}
-
 export default {
-  async fetch(request: Request, env: Env): Promise<Response> {
+  async fetch(request: Request): Promise<Response> {
     const reqHeaders = new Headers(request.headers);
     const response: {
       body: BodyInit | null;
@@ -47,7 +36,7 @@ export default {
         url == "robots.txt"
       ) {
         const invalid = !(request.method == "OPTIONS" || url.length === 0);
-        response.body = await getHelp(env, new URL(request.url));
+        response.body = await getHelp(new URL(request.url));
         response.contentType = "text/html";
         response.status = invalid ? 400 : 200;
       } else {
@@ -91,7 +80,6 @@ export default {
         response.text = fetchResponse.statusText;
         response.body = fetchResponse.body;
 
-        await increment(env);
       }
     } catch (err) {
       if (err instanceof Error) {
@@ -126,11 +114,11 @@ function fixUrl(url: string) {
   }
 }
 
-async function getHelp(env: Env, url: URL) {
+async function getHelp(url: URL) {
   return `<!DOCTYPE html>
   <html>
 	<head>
-	  <title>Streetwriters CORS Proxy</title>
+	  <title>Safone's CORS Proxy</title>
 	  <style>
 		body {
 		  font-family: "Courier New", Courier, monospace;
@@ -138,46 +126,27 @@ async function getHelp(env: Env, url: URL) {
 	  </style>
 	</head>
 	<body>
-	  <p>Welcome to Streetwriters CORS Proxy!</p>
+	  <p>Welcome to Safone's CORS Proxy!</p>
 	  <p>
-		You can use this proxy to bypass CORS in the browser. It is recommended
-		that you deploy your own instance both for privacy reasons and to reduce
-		the load on a single instance.
+    This enables cross-origin requests to anywhere.
+    <br>
+		You can use this proxy to bypass CORS in the browser.
 	  </p>
 	  <p>USAGE:</p>
 	  <p>
 		&nbsp;&nbsp;&nbsp;&nbsp;${url.protocol}//${url.hostname}/&lt;url-to-resource&gt;
 	  </p>
 	  <p>EXAMPLES:</p>
-	  <p>&nbsp;&nbsp;&nbsp;&nbsp;${url.protocol}//${
-    url.hostname
-  }/https://api.github.com/repos/streetwriters/notesnook/releases/tags/v2.2.4</p>
-	  <p>&nbsp;&nbsp;&nbsp;&nbsp;${url.protocol}//${
-    url.hostname
-  }/https://api.github.com/repos/streetwriters/notesnook/releases</p>
-	  <p>
-		<a
-		  href="https://deploy.workers.cloudflare.com/?url=https://github.com/streetwriters/cors"
-		  >Deploy your own!</a
-		>
-		| <a href="https://github.com/streetwriters/cors">GitHub repository</a>
-	  </p>
-	  <hr/>
-	  <p>
-		Bypassed ${await totalRequests(env)} requests!
-	  </p>
+	  <p>&nbsp;&nbsp;&nbsp;&nbsp;${url.protocol}//${url.hostname}/https://api.github.com/repos/AsmSafone/SafoneAPI</p>
+	  <p>&nbsp;&nbsp;&nbsp;&nbsp;${url.protocol}//${url.hostname}/https://api.github.com/repos/AsmSafone/SafoneAPI/releases</p>
+    <br/>
+    <footer>
+      <p align="center">
+        <a href="https://github.com/AsmSafone">GitHub Repository</a>
+        | <a href="https://buymeacoffee.com/safone">Buy Me a Coffee!</a>
+      </p>
+    </footer>
 	</body>
   </html>
   `;
-}
-
-async function increment(env: Env) {
-  if (!env.ANALYTICS) return;
-  let count = parseInt((await env.ANALYTICS.get("total_requests")) || "0");
-  await env.ANALYTICS.put("total_requests", (++count).toFixed());
-}
-
-async function totalRequests(env: Env) {
-  if (!env.ANALYTICS) return 0;
-  return await env.ANALYTICS.get("total_requests");
 }
